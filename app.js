133b427492724051376c3189d6c89990bb322007
Marco Di Summa <mdisumma@yahoo.com>
2024-07-23 18:40:41 +0100

    tiket 1

--- app.js
+++ app.js
@@ -29,7 +29,13 @@ res.json({
 
 /* Write a request handler to return the correct response when a `GET` request is received to `/astronauts`. Choose the appropriate 
 function from the imported functions at the top of the `app.js` to get your data. */
-
+app.get("/astronauts", async (request, response) => {
+  const data = {
+    success: response.statusMessage,
+    payload: await getAstronauts(),
+  };
+  response.send(data);
+});
 // Task 2
 
 /* Write a request handler to return the correct response and perform the correct action when a `POST` request is received to 
